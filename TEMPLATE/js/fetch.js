d982434d0e211f90a644763bc13ec646f2035416
qintharganteng <qintharalmaliki@gmail.com>
2024-04-26 17:56:11 +0700

    dua

--- TEMPLATE/js/fetch.js
+++ TEMPLATE/js/fetch.js
@@ -1,5 +1,5 @@
 import { get } from "https://bukulapak.github.io/api/process.js"; 
-let urlAPI = "https://ws-indra2024-878f7e6fab92.herokuapp.com/presensi";
+let urlAPI = " https://ws-qinthar2024-48d72259230b.herokuapp.com/presensi";
 get(urlAPI,isiTablePresensi);
 function isiTablePresensi(results){
     console.log(results);
