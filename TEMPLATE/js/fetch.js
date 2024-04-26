c0145765a8092870a16f968667b42b515ae3e79b
qintharganteng <qintharalmaliki@gmail.com>
2024-04-26 17:58:28 +0700

    tiga

--- TEMPLATE/js/fetch.js
+++ TEMPLATE/js/fetch.js
@@ -1,6 +1,12 @@
-import { get } from "https://bukulapak.github.io/api/process.js"; 
-let urlAPI = " https://ws-qinthar2024-48d72259230b.herokuapp.com/presensi";
+
+import { get } from "https://bukulapak.github.io/api/process.js";
+import { setInner } from "https://bukulapak.github.io/element/process.js";
+let urlAPI = "https://ws-qinthar2024-48d72259230b.herokuapp.com/presensi";
 get(urlAPI,isiTablePresensi);
 function isiTablePresensi(results){
     console.log(results);
+    results.forEach(isiRow);
+}
+function isiRow(value){
+    console.log(value)
 }
