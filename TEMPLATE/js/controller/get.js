dee008227507776476541681b07f30b6361419be
qintharganteng <qintharalmaliki@gmail.com>
2024-04-26 18:16:34 +0700

    lima

--- TEMPLATE/js/controller/get.js
+++ TEMPLATE/js/controller/get.js
@@ -1,6 +1,6 @@
 import { addInner } from "https://bukulapak.github.io/element/process.js";
 import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
-import { isiTabel } from "../temp/table.js";
+import { isiTabel } from "../TEMPLATE/temp/table.js";
 export function isiTablePresensi(results) {
     results.forEach(isiRow);
 }
